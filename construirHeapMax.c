97a8d38f8e48ffe023c54a1ccb29c1b343effd62
CamaradaRomeuTB <romeutb2007@gmail.com>
2026-05-06 22:26:50 -0300

    adicionando construção de heap binário de máximo em vetor

---
+++ construirHeapMax.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void maxheapfy(int *v, int i, int n)
+{
+	int esq = i * 2 + 1;
+	int dir = i * 2 + 2;
+	int maior;
+	int temp;
+
+	if(esq <= n && v[esq] > v[i]) maior = esq;
+	else maior = i;
+	
+	if(dir <= n && v[dir] > v[maior]) maior = dir;
+
+	if(maior != i)
+	{
+		temp = v[i];
+		v[i] = v[maior];
+		v[maior] = temp;
+		maxheapfy(v, maior, n);
+	}
+}
+
+void construirHeap(int *v, int n)
+{
+	for(int i = n/2 + 1; i >= 0; i--) maxheapfy(v, i, n);
+}
+
+void main()
+{
+	int v[7] = {4, 19, 32, 1, 31, 76, 54};
+	construirHeap(v, 6);
+
+	for(int i = 0; i < 7; i++) printf("%d ", v[i]);
+	printf("\n");
+}
